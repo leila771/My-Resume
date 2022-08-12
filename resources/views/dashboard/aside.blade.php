14858591026dd411b650f0afbca8890edebcc08a
Steffen414 <landry.epesse@movingmedia.fr>
2022-08-12 18:49:52 +0100

    0

--- resources/views/dashboard/aside.blade.php
+++ resources/views/dashboard/aside.blade.php
@@ -38,7 +38,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 @if (Auth::user()->isAdmin())
-                
+                 
                     <li class="nav-item menu-open">
                         <a href="#" class="nav-link active">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
